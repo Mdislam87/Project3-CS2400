d5a620adbcd682cc30b2baeb73614217cec29bc6
Van Huynh <vthuynh@cpp.edu>
2021-10-25 23:41:18 -0700

    finish implementing createTree2 in DriverBT

--- DriverBT.java
+++ DriverBT.java
@@ -76,8 +76,30 @@ public class DriverBT
 	/**-------------------------------------------------------------------- */
 	/**Task 4: Create a tree case as shown in assignment 3*/
 	public static void createTree2(BinaryTree<String> tree)
-	{ 
+	{
+		// Leaves
+		BinaryTree<String> fTree = new BinaryTree<>("F");
+		BinaryTree<String> gTree = new BinaryTree<>("G");
+		BinaryTree<String> hTree = new BinaryTree<>("H");
 
+		// Subtrees:
+		BinaryTree<String> dTree = new BinaryTree<>("F", fTree, null);
+		BinaryTree<String> eTree = new BinaryTree<>("B", gTree, hTree);
+		BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+		BinaryTree<String> bTree = new BinaryTree<>("B", null,null);
+
+		// Root
+		tree.setTree("A",bTree,cTree);
+
+		System.out.println("\nGiven Tree:\n");
+		System.out.println("     A      ");
+		System.out.println("   /   \\  ");
+		System.out.println("  B     C  ");
+		System.out.println("	   / \\  ");
+		System.out.println("      D   E");
+		System.out.println("     /   /  \\ ");
+		System.out.println("    F   G    H");
+		System.out.println();
 	} // end createTree2
 
 }  // end DriverBT
